c1310f111d5e82f835dfd7ed4fd929c47ae37bde
John Fay <john.fay@duke.edu>
2020-10-23 14:02:23 -0400

    Add alt jupyter link

--- Open_Jupyter.bat
+++
@@ -1,4 +0,0 @@
-@set the_env=gis
-call "C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\activate.bat" "%the_env%"
-call "%localappdata%\ESRI\conda\envs\%the_env%\scripts\jupyter-notebook.exe" %cd% 
-call "C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\deactivate.bat"
---
+++ Open_Jupyter_arcgis.bat
@@ -0,0 +1,3 @@
+call "C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\activate.bat" "arcgispro-py3"
+call "jupyter-notebook.exe" %cd% 
+call "C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\deactivate.bat"
